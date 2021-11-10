865d17495c1e96883c780e3471111b4558098f78
Moeen Mahmud <write2fahim007@gmail.com>
2021-11-10 13:59:14 +0600

    GET products

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -8,6 +10,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Mongo Client
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@hobbylobbycluster.j98az.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Mongo Connection
+async function run() {
+  try {
+    await client.connect();
+
+    // Database collection
+    const database = client.db("hobbyLobbyDatabase");
+    const productCollection = database.collection("products");
+
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 // Testing server endpoint
 app.get("/", (req, res) => {
   res.send("Niche product website server is running");
